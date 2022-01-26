14cabfd91eac79598cdcb303149f318766a2a5a8
Brandon Bednar <bwbednar@ksu.edu>
2022-01-25 20:36:06 -0600

    Add project files.

---
+++ CIS580_Project/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace CIS580_Project
+{
+    public static class Program
+    {
+        [STAThread]
+        static void Main()
+        {
+            using (var game = new FireworkGame())
+                game.Run();
+        }
+    }
+}
